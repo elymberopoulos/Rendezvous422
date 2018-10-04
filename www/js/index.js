a2844bdf26658c597fc44d8f443aee8d79da1732
Eric <elymberopoulos@luc.edu>
2018-10-04 13:51:39 -0500

    added code implementing an indexedDB. Also working on a function that can save latitude and longitude to the indexedDB.

--- www/js/index.js
+++ www/js/index.js
@@ -27,16 +27,71 @@ var app = {
     // Bind any cordova events here. Common events are:
     // 'pause', 'resume', etc.
     onDeviceReady: function() {
-        
+
         var geoOptions = {
           timeout: 10000,
           enableHighAccuracy: false,
           maximumAge: 10000
         }
-        navigator.geolocation.getCurrentPosition(app.geoSuccess, app.geoFail, geoOptions);   
-         
+        navigator.geolocation.getCurrentPosition(app.geoSuccess, app.geoFail, geoOptions);
+
+        //INDEXEDDB VARIABLES
+        var dataBase;
+        var dataBaseName = "coordinatesDB";
+        var dataBaseVersion = 1;
+        var indexedDBSupported = false;
+        if("indexedDB" in window){
+          indexedDBSupported = true;
+          console.log("IndexedDB supported");
+        }else{
+          console.log("No indexedDB support");
+        }
+        if(indexedDBSupported){
+          var openDB = window.indexedDB.open(dataBaseName, dataBaseVersion);
+          openDB.onupgradeneeded = function(event){
+            console.log("Upgrading DB");
+            var upgradeDB = event.target.reult;
+            if(!upgradeDB.objectStoreNames.contains("coordinatesDBos")){
+              upgradeDB.createObjectStore("coordinatesDBos", {keyPath: "Favorite Locations"});
+              console.log("New object store created");
+            }
+            if(!upgradeDB.objectStoreNames.contains("coordinatesDBos")){
+              upgradeDB.createObjectStore("coordinatesDBos2", {keyPath: "Favorite Locations"});
+              console.log("New object store 2 created");
+            }
+          }
+          openDB.onsuccess = function(event){
+            console.log("DB successful");
+            db = event.target.result;
+          }
+          openDB.onerror = function(event){
+            console.log("DB ERROR");
+            console.dir(event);
+          }
+        }
+        var dbTransaction = dataBase.transaction("coordinatesDB","readwrite").objectStore("coordinatesDB");
+        var coordinatesDBAccessor = dbTransaction.objectStore("coordinatesDB");
+        //INDEXEDDB END=========================================================
+
         //this.receivedEvent('deviceready');
     },
+    saveCoordinates: function(){
+      //use navigator to get latitude and longitude and store
+      //in it indexedDB with form value for key.
+
+      //var saveKey = document.getElementById(""); // TODO: FIND WAY TO GET document
+                                                  //element value for key in data store
+      navigator.geolocation.getCurrentPosition(function(position){
+        var long = position.coords.longitude;
+        var lat = position.coords.latitude;
+        var storeData = {
+          longitude:long,
+          latitude:lat
+        }
+        var addData = coordinatesDBAccessor.add(storeData,saveKey);
+    }
+    ,app.geoFail
+  )},
 
     geoSuccess: function(position){
         var longitude = position.coords.longitude;
@@ -60,7 +115,6 @@ var app = {
 
     geoFail: function(error){
         console.log("the code is " + error.code + ". \n" + "message: " + error.message);
-        
     },
     // Update DOM on a Received Event
     /*receivedEvent: function(id) {
