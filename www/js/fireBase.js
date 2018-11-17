61b8d0d44c6c38e0743ced63810ae79507828fa5
Eric <elymberopoulos@luc.edu>
2018-11-17 16:58:21 -0600

    Firebase login works with Google authentication. Index.html buttons are hidden until network connection is detected. Implemented basic network connectivity checks with Cordova plugin.

--- www/js/fireBase.js
+++ www/js/fireBase.js
@@ -1,58 +1,74 @@
+// https://rendezvous.page.link
 function fireBaseInit() {
-  // Initialize Firebase
-  var config = {
-    apiKey: "",
-    authDomain: "rendezvous-219221.firebaseapp.com",
-    databaseURL: "https://rendezvous-219221.firebaseio.com",
-    projectId: "rendezvous-219221",
-    storageBucket: "rendezvous-219221.appspot.com",
-    messagingSenderId: "343762694139"
-  };
-  firebase.initializeApp(config);
 
-  const db = firebase.database();
-  console.log(db);
-  const googleProvider = new firebase.auth.GoogleAuthProvider();
-  const facebookProvider = new firebase.auth.FacebookAuthProvider();
+  var networkState = navigator.connection.type;
+  var loginBtn = document.getElementById("loginBtn");
+  var registerBtn = document.getElementById("registerBtn");
+  var mapPageBtn = document.getElementById("mapPageBtn");
 
-  var googleLogin = document.getElementById('loginBtn').addEventListener('click', googleSignIn);
+  if (networkState !== Connection.NONE) {
+    var config = {
+      //INSERT CONFIG VALUES HERE FROM FIREBASE
+      //REMOVED FOR SECURITY
+    };
+    firebase.initializeApp(config);
 
-  function googleSignIn() {
+    const db = firebase.database();
+    console.log(db);
+    var googleLogin = document.getElementById('loginBtn').addEventListener('click', googleSignIn);
+    const googleProvider = new firebase.auth.GoogleAuthProvider();
+    const facebookProvider = new firebase.auth.FacebookAuthProvider();
+    
+    loginBtn.style.visibility="visible";
+    registerBtn.style.visibility="visible";
+    mapPageBtn.style.visibility="visible";
 
-    console.log("Google login clicked.");
-    console.log(googleProvider);
-    //firebase.auth().languageCode = firebase.auth().useDeviceLanguage();
-    return firebase.auth().signInWithRedirect(googleProvider);
-    // then(function(result) {
-    //
-    //   var token = result.credential.accessToken;
-    //
-    //   var user = result.user;
-    //
-    // }).catch(function(error) {
-    //   var errorCode = error.code;
-    //   var errorMessage = error.message;
-    //   var email = error.email;
-    //   var credential = error.credential;
-    //
-    // });
+    function googleSignIn() {
+      if (networkState !== Connection.NONE) {
+        console.log("Google login clicked.");
+        console.log(googleProvider);
+        //firebase.auth().languageCode = firebase.auth().useDeviceLanguage();
+        return firebase.auth().signInWithRedirect(googleProvider);
+      } else {
+        alert("No network connection detected!");
+      }
+      // then(function(result) {
+      //
+      //   var token = result.credential.accessToken;
+      //
+      //   var user = result.user;
+      //
+      // }).catch(function(error) {
+      //   var errorCode = error.code;
+      //   var errorMessage = error.message;
+      //   var email = error.email;
+      //   var credential = error.credential;
+      //
+      // });
+    }
+  
+    function facebookSignIn() {
+      console.log("Facebook login clicked.");
+      console.log(facebookProvider);
+      firebase.auth().languageCode = firebase.auth().useDeviceLanguage();
+      firebase.auth().signInWithPopup(facebookProvider).then(function (result) {
+  
+        var token = result.credential.accessToken;
+  
+        var user = result.user;
+  
+      }).catch(function (error) {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        var email = error.email;
+        var credential = error.credential;
+  
+      });
+    }
   }
-  function facebookSignIn() {
-    console.log("Facebook login clicked.");
-    console.log(facebookProvider);
-    firebase.auth().languageCode = firebase.auth().useDeviceLanguage();
-    firebase.auth().signInWithPopup(facebookProvider).then(function(result) {
-
-      var token = result.credential.accessToken;
-
-      var user = result.user;
-
-    }).catch(function(error) {
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      var email = error.email;
-      var credential = error.credential;
-
-    });
+  else{
+    alert("This application requires a network connection. No network connection detected.");
   }
+
+
 }
