09da7e33bcbb9ec1c89159a3bc4f47bf982378c3
Pat M <patdjm7@gmail.com>
2018-11-27 15:54:37 -0600

    Just made small changes to my config.

--- www/js/fireBase.js
+++ www/js/fireBase.js
@@ -9,7 +9,12 @@ function fireBaseInit() {
 
   if (networkState !== Connection.NONE) {
     var config = {
-      
+      apiKey: "AIzaSyCVoSGosWTkRgL69T4Au3aGrCBgViBuI1M",
+      authDomain: "rendezvous-219221.firebaseapp.com",
+      databaseURL: "https://rendezvous-219221.firebaseio.com",
+      projectId: "rendezvous-219221",
+      storageBucket: "rendezvous-219221.appspot.com",
+      messagingSenderId: "343762694139"
     };
     firebase.initializeApp(config);
 
